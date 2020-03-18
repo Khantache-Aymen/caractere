125944a20cbe6eb34bfd0384ba10609d802fcfe5
Khantache-Aymen <khantacheaymenfadiomar@gmail.com>
2020-03-18 21:32:56 +0100

    j'ai rien changé

---
+++ caractere.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	char ch,chplus1;
+	unsigned char chr,chr1;
+	ch=123;
+	chplus1=ch+1;
+	chr=0;
+	chr1=chr+1;
+	printf("suivant larithmetique des caracteres:\n");
+	printf("ch=%d,chplus1=%d\n",ch,chplus1);
+	printf("ch=%c,chplus1=%c\n",ch,chplus1);
+	printf("chr=%d,chr1=%d\n",chr,chr1);
+	printf("chr=%c,chr1=%c\n",chr,chr1);
+}
